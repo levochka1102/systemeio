60574aa8ff91c710989f9795e647d95cebb97e5d
Lev <mlev1102@gmail.com>
2024-04-21 14:27:47 +0500

    [fix] fix migration, remove product table creating which duplicates previous migration version

--- migrations/Version20240421081029.php
+++ migrations/Version20240421081029.php
@@ -21,9 +21,7 @@ final class Version20240421081029 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE coupon_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE product_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE coupon (id INT NOT NULL, code VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE product (id INT NOT NULL, name VARCHAR(255) NOT NULL, price INT NOT NULL, currency_code VARCHAR(3) NOT NULL, PRIMARY KEY(id))');
     }
 
     public function down(Schema $schema): void
@@ -31,8 +29,6 @@ final class Version20240421081029 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE coupon_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE product_id_seq CASCADE');
         $this->addSql('DROP TABLE coupon');
-        $this->addSql('DROP TABLE product');
     }
 }
