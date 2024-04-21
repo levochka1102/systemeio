5833a30895661c9702f4e2193cbb77bd57015f44
Lev <mlev1102@gmail.com>
2024-04-21 14:54:36 +0500

    [fix] fix migrations; schema public already exists

--- migrations/Version20240421081029.php
+++ migrations/Version20240421081029.php
@@ -27,7 +27,6 @@ final class Version20240421081029 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
         $this->addSql('DROP SEQUENCE coupon_id_seq CASCADE');
         $this->addSql('DROP TABLE coupon');
     }
